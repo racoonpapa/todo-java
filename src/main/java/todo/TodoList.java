0bcb5389f3f7dad7d4169782d5b50a591017e9d5
racoonpapa <racoonpapa@naver.com>
2023-09-16 11:39:52 +0900

    feat: Change container to HashMap instead of ArrayList

--- src/main/java/todo/TodoList.java
+++ src/main/java/todo/TodoList.java
@@ -7,11 +7,12 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class TodoList {
-    private final ArrayList<TodoItem> items;
+    private final HashMap<String, TodoItem> items = new HashMap<>();
 
     private static final Gson gson = new GsonBuilder()
             .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
@@ -20,29 +21,27 @@ public class TodoList {
 
     public static TodoList fromJson(String json) {
         try {
-            Type todoListType = new TypeToken<ArrayList<TodoItem>>() {}.getType();
-            ArrayList<TodoItem> items = gson.fromJson(json, todoListType);
-            return new TodoList(items);
+            TodoList list = new TodoList();
+
+            JsonArray jsonArray = JsonParser.parseString(json).getAsJsonArray();
+            for(JsonElement element: jsonArray) {
+                TodoItem item = gson.fromJson(element, TodoItem.class);
+                list.items.put(item.itemId, item);
+            }
+
+            return list;
         } catch(Exception e) {
             return null;
         }
     }
 
     public String toJson() {
-        Type todoListType = new TypeToken<ArrayList<TodoItem>>() {}.getType();
-        return gson.toJson(items, todoListType);
-    }
-
-    public TodoList() {
-        items = new ArrayList<>();
-    }
-
-    public TodoList(ArrayList<TodoItem> items) {
-        this.items = items;
+        Type todoListType = new TypeToken<Collection<TodoItem>>() {}.getType();
+        return gson.toJson(items.values(), todoListType);
     }
 
     @Override
     public String toString() {
-        return items.stream().map(TodoItem::toString).collect(Collectors.joining("\n"));
+        return items.values().stream().map(TodoItem::toString).collect(Collectors.joining("\n"));
     }
 }
